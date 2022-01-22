76440e524384885eb0c79b5982f01feb2733e408
Cheap-Ship <56252676+Cheap-Ship@users.noreply.github.com>
2022-01-22 16:49:00 +0000

    process.env.PORT

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 8080;
 
 const expressSwagger = require('express-swagger-generator')(app); 
 const options = require('./config/swagger'); 
